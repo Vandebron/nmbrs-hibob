c8773185e36a0cd5707bf8ad53c8704b84615fd9
SamTheisens <1911436+SamTheisens@users.noreply.github.com>
2022-01-16 16:11:41 +0800

    Write all PDFs into zipfile
    branch:

--- export.py
+++ export.py
@@ -3,6 +3,10 @@ import requests
 import xml.etree.ElementTree as ET
 import base64
 from halo import Halo
+from progress.bar import ShadyBar
+from dataclasses import dataclass
+from zipfile import ZipFile
+
 
 parser = argparse.ArgumentParser(description='Export salary slip PDFs from Visma Nmbrs into Hibob')
 parser.add_argument('--user', '-u', help=f'API user', default='IT@vandebron.nl')
@@ -18,6 +22,15 @@ password_arg = args.token
 run_arg = args.run
 year_arg = args.year
 company_id = args.company
+
+
+@dataclass
+class RunInfo:
+    id: str
+    number: str
+    description: str
+    period_start: str
+    period_end: str
 
 
 def create_request(user, password, payload):
@@ -94,30 +107,48 @@ if len(companies) > 1:
     spinner.fail(f"More than one company found. Choose between {companies} via --company.")
 
 company_id = companies.pop().find('./cs:ID', namespaces=ns).text
-if not run_arg:
-    spinner.text = f"Getting run information for company {company_id} and year {year_arg}"
-    tree = do_request(get_runs(year_arg))
-    spinner.succeed(f"Got run information for company {company_id} and year {year_arg}")
-    run_infos = tree.findall('.//cs:RunInfo', namespaces=ns)
 
-    for run_info in run_infos:
-        number = run_info.find('./cs:ID', namespaces=ns).text
-        description = run_info.find('./cs:Description', namespaces=ns).text
-        period_start = run_info.find('./cs:PeriodStart', namespaces=ns).text
-        period_end = run_info.find('./cs:PeriodEnd', namespaces=ns).text
-        print(f'{number} {description} {period_start}-{period_end}')
+
+def to_run_info(element) -> RunInfo:
+    id = element.find('./cs:ID', namespaces=ns).text
+    number = element.find('./cs:Number', namespaces=ns).text
+    description = element.find('./cs:Description', namespaces=ns).text
+    period_start = element.find('./cs:PeriodStart', namespaces=ns).text
+    period_end = element.find('./cs:PeriodEnd', namespaces=ns).text
+    return RunInfo(id, number, description, period_start, period_end)
+
+
+def get_run_info(run):
+    global spinner
+    spinner.text = f"Getting run information for company {company_id} and year {year_arg}"
+    result = do_request(get_runs(year_arg))
+    spinner.succeed(f"Got run information for company {company_id} and year {year_arg}")
+    if run:
+        info = result.find(f'.//cs:RunInfo/[cs:ID="{run}"]', namespaces=ns)
+        return to_run_info(info)
+    run_infos = result.findall('.//cs:RunInfo', namespaces=ns)
+    for run_info_element in run_infos:
+        r = to_run_info(run_info_element)
+        print(f'{r.id} {r.description} {r.period_start}-{r.period_end}')
     exit()
 
+
+run_info = get_run_info(run_arg)
+
+spinner.text = f"Finding employees for {run_info.number} {run_info.description}"
 tree = do_request(get_employees(run_arg, year_arg))
 employees = tree.findall('.//cs:EmployeeId', namespaces=ns)
-spinner.succeed(f"Found {len(employees)} employees for run {run_arg}, year {year_arg}")
+spinner.succeed(f"Found {len(employees)} employees for run {run_info.description}")
 
-for i, employee in enumerate(employees):
-    employee_id = employee.text
-    spinner.start(f"{i} Fetching salary slip for employee {employee_id}")
-    tree = do_request(get_payslip(employee_id, run_arg, year_arg))
-    pdf = tree.find('.//cs:PDF', namespaces=ns)
-    pdfBinary = base64.b64decode(pdf.text)
-    with open(f'{employee_id}.pdf', "wb") as slip_file:
-        slip_file.write(pdfBinary)
-        spinner.succeed(f"{i} Fetched salary slip for employee {employee_id}")
+zip_file_name = f"run_{run_info.number}_{year_arg}.zip"
+with ShadyBar("Fetching salary slip for employees", max=len(employees)) as bar:
+    with ZipFile(zip_file_name, 'w') as zip_file:
+        for i, employee in enumerate(employees):
+            employee_id = employee.text
+            tree = do_request(get_payslip(employee_id, run_arg, year_arg))
+            pdf = tree.find('.//cs:PDF', namespaces=ns)
+            pdfBinary = base64.b64decode(pdf.text)
+            zip_file.writestr(f"{employee_id}_{run_info.number}_{year_arg}.pdf", pdfBinary)
+            bar.next()
+
+spinner.succeed(f"Wrote results to {zip_file_name}")
